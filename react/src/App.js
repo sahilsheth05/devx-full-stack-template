bb8f53588311c5125ead723b83799cd8c04e9800
sahilsheth05 <sahilnsheth@gmail.com>
2024-11-19 20:06:29 -0800

    Update App.js

--- react/src/App.js
+++ react/src/App.js
@@ -5,7 +5,7 @@ import { useState } from "react";
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">Sahil is awesome</header>
     </div>
   );
 }
